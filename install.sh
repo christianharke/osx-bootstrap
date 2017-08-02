b546d99d3e20850f8500e3f0e0188ae8305987ec
Christian Harke <ch.harke@gmail.com>
2017-08-02 22:13:50 +0200

    Clone git repo

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@
 dir="$HOME/repos"
 mkdir -p $dir
 cd $dir || exit
-# git clone --recursive https://github.com/christianharke/osx-bootstrap.git
-cd osx-bootstrap || exit
+git clone --recursive https://github.com/christianharke/osx-bootstrap.git
+cd osx-bootstrap || exit
 bash bootstrap.sh
