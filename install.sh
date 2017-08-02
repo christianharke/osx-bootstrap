b1c8b530300965d28ebff8cf8d90b7c7e2ae3d4c
Christian Harke <ch.harke@gmail.com>
2017-08-02 23:55:42 +0200

    Install command line tools in advance

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 dir="$HOME/repos"
 mkdir -p $dir
 cd $dir || exit
+xcode-select --install
 git clone --recursive https://github.com/christianharke/osx-bootstrap.git
 cd osx-bootstrap || exit
 bash bootstrap.sh
